f58fe1a15364c9611ef1b3a31e1d3eaf7990a324
Almog Gabay <theninesky@gmail.com>
2022-05-19 10:55:07 +0300

    Fix fetchQuotes #3

--- scripts/fetch-static.mjs
+++ scripts/fetch-static.mjs
@@ -3,21 +3,25 @@ import { mkdir, writeFile } from 'fs/promises'
 import axios from 'axios'
 
 const path = 'public/json'
+const axiosOptions = { transformResponse: res => res }
 
-axios.defaults.transformResponse = res => res
 mkdir(path, { recursive: true })
 process.on('beforeExit', () => console.log('Fetched all static assets.'))
 
 const fetchInfo = async () => {
-  const { data } = await axios.get('https://baconipsum.com/api/?type=meat-and-filler&paras=200')
+  const { data } = await axios.get('https://baconipsum.com/api/?type=meat-and-filler&paras=200', axiosOptions)
 
   writeFile(`${path}/info.json`, data)
 }
 
 const fetchQuotes = async () => {
-  const { data } = await axios.get('https://zenquotes.io/api/quotes')
+  const [{ data: data1 }, { data: data2 }, { data: data3 }] = await Promise.all([
+    axios.get('https://zenquotes.io/api/quotes'),
+    axios.get('https://zenquotes.io/api/quotes'),
+    axios.get('https://zenquotes.io/api/quotes')
+  ])
 
-  writeFile(`${path}/quotes.json`, data)
+  writeFile(`${path}/quotes.json`, JSON.stringify([...data1, ...data2, ...data3]))
 }
 
 fetchInfo()
