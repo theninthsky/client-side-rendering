67b7b1d24d83a2438153aea43f037f488a4e6597
Almog Gabay <theninesky@gmail.com>
2022-05-25 15:44:01 +0300

    Add weak offline support

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,23 @@
-self.addEventListener('install', self.skipWaiting)
+const networkFirst = async event => {
+  try {
+    const res = await fetch(event.request)
+    const cache = await caches.open('csr')
 
-self.addEventListener('fetch', () => {})
+    event.waitUntil(cache.put(event.request, res.clone()))
+
+    return res
+  } catch (err) {
+    return caches.match(event.request)
+  }
+}
+
+self.addEventListener('install', event => {
+  const cache = caches.open('csr')
+
+  event.waitUntil(cache.add('./'))
+  self.skipWaiting()
+})
+
+self.addEventListener('fetch', event => {
+  if (event.request.url === self.registration.scope) event.respondWith(networkFirst(event))
+})
