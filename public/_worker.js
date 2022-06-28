87d4d8a5dd3a92190ff0d0a9fda8829ba86f246e
Almog Gabay <theninesky@gmail.com>
2022-06-28 07:41:20 +0300

    Add prerender-io worker

---
+++ public/_worker.js
@@ -0,0 +1,112 @@
+const { API_KEY } = process.env
+const BOT_AGENTS = [
+  'googlebot',
+  'yahoo! slurp',
+  'bingbot',
+  'yandex',
+  'baiduspider',
+  'facebookexternalhit',
+  'twitterbot',
+  'rogerbot',
+  'linkedinbot',
+  'embedly',
+  'quora link preview',
+  'showyoubot',
+  'outbrain',
+  'pinterest/0.',
+  'developers.google.com/+/web/snippet',
+  'slackbot',
+  'vkshare',
+  'w3c_validator',
+  'redditbot',
+  'applebot',
+  'whatsapp',
+  'flipboard',
+  'tumblr',
+  'bitlybot',
+  'skypeuripreview',
+  'nuzzel',
+  'discordbot',
+  'google page speed',
+  'qwantify',
+  'pinterestbot',
+  'bitrix link preview',
+  'xing-contenttabreceiver',
+  'chrome-lighthouse',
+  'telegrambot'
+]
+const IGNORE_EXTENSIONS = [
+  '.js',
+  '.css',
+  '.xml',
+  '.less',
+  '.png',
+  '.jpg',
+  '.jpeg',
+  '.gif',
+  '.pdf',
+  '.doc',
+  '.txt',
+  '.ico',
+  '.rss',
+  '.zip',
+  '.mp3',
+  '.rar',
+  '.exe',
+  '.wmv',
+  '.doc',
+  '.avi',
+  '.ppt',
+  '.mpg',
+  '.mpeg',
+  '.tif',
+  '.wav',
+  '.mov',
+  '.psd',
+  '.ai',
+  '.xls',
+  '.mp4',
+  '.m4a',
+  '.swf',
+  '.dat',
+  '.dmg',
+  '.iso',
+  '.flv',
+  '.m4v',
+  '.torrent',
+  '.woff',
+  '.ttf',
+  '.svg',
+  '.webmanifest'
+]
+
+const prerenderRequest = request => {
+  const { url, headers } = request
+  const prerenderUrl = `https://service.prerender.io/${url}`
+  const headersToSend = new Headers(headers)
+
+  headersToSend.set('X-Prerender-Token', API_KEY)
+
+  const prerenderRequest = new Request(prerenderUrl, {
+    headers: headersToSend,
+    redirect: 'manual'
+  })
+
+  return fetch(prerenderRequest)
+}
+
+addEventListener('fetch', event => {
+  const { request } = event
+  const requestUserAgent = (request.headers.get('User-Agent') || '').toLowerCase()
+
+  if (!BOT_AGENTS.includes(requestUserAgent)) return
+
+  const { pathname, hostname } = new URL(request.url)
+  const xPrerender = request.headers.get('X-Prerender')
+  const pathName = pathname.toLowerCase()
+  const ext = pathName.slice(pathName.lastIndexOf('.') || pathName.length)
+
+  if (!IGNORE_EXTENSIONS.includes(ext) && !xPrerender && hostname === 'client-side-rendering.pages.dev') {
+    event.respondWith(prerenderRequest(request))
+  }
+})
