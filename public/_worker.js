b898133f0fb9052e09a4328b203132fd5c96dcd3
Almog Gabay <theninesky@gmail.com>
2022-07-21 14:33:19 +0300

    Attempt to add clouflare worker

---
+++ public/_worker.js
@@ -0,0 +1,62 @@
+const BOT_AGENTS = [
+  'bingbot',
+  'yahoo! slurp',
+  'yandex',
+  'baiduspider',
+  'facebookexternalhit',
+  'twitterbot',
+  'rogerbot',
+  'linkedinbot',
+  'embedly',
+  'quora link preview',
+  'showyoubot',
+  'outbrain',
+  'pinterest/0.',
+  'developers.google.com/+/web/snippet',
+  'slackbot',
+  'vkshare',
+  'w3c_validator',
+  'redditbot',
+  'applebot',
+  'whatsapp',
+  'flipboard',
+  'tumblr',
+  'bitlybot',
+  'skypeuripreview',
+  'nuzzel',
+  'discordbot',
+  'google page speed',
+  'qwantify',
+  'pinterestbot',
+  'bitrix link preview',
+  'xing-contenttabreceiver',
+  'chrome-lighthouse',
+  'telegrambot'
+]
+
+const fetchPrerendered = async (request, env) => {
+  const { url, headers } = request
+  const prerenderUrl = `https://service.prerender.io/${url}`
+  const headersToSend = new Headers(headers)
+
+  headersToSend.set('X-Prerender-Token', env.PRERENDER_IO_KEY)
+
+  const prerenderRequest = new Request(prerenderUrl, {
+    headers: headersToSend,
+    redirect: 'manual'
+  })
+
+  const { status, body } = await fetch(prerenderRequest)
+
+  return new Response(body, { status })
+}
+
+export default {
+  fetch(request, env) {
+    const userAgent = (request.headers.get('User-Agent') || '').toLowerCase()
+
+    if (BOT_AGENTS.includes(userAgent)) return fetchPrerendered(request, env)
+
+    return env.ASSETS.fetch(request)
+  }
+}
