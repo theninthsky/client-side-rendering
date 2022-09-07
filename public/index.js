e1b4f831a27120d52ebaaa4485c31190c35b16b1
Almog Gabay <theninesky@gmail.com>
2022-09-07 23:36:30 +0300

    Attempt to fix font preload #3

--- public/index.js
+++ public/index.js
@@ -14,7 +14,7 @@ module.exports = pages => `
 
       <link rel="shortcut icon" href="/icons/favicon.ico">
       <link rel="manifest" href="/manifest.json">
-      <link rel="preload" href="/fonts/montserrat.woff2" as="font" type="font/woff2" crossorigin="use-credentials">
+      <link rel="preload" href="/fonts/montserrat.woff2" as="font" type="font/woff2" crossorigin>
 
       <title></title>
 
