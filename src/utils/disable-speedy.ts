2666bc33b99c0d62399094013d08bc016f6a08fc
Almog Gabay <theninesky@gmail.com>
2022-09-08 01:23:35 +0300

    Change speedy disable condition

--- src/utils/disable-speedy.ts
+++ src/utils/disable-speedy.ts
@@ -1,6 +1,3 @@
 import { sheet } from '@emotion/css'
 
-// Check if the root node has any children to detect if the app has been preprendered
-// speedy is disabled when the app is being prerendered so that styles render into the DOM
-// speedy is significantly faster though so it should only be disabled during prerendering
-if (document.getElementById('root')?.hasChildNodes()) sheet.speedy(false)
+if (navigator.userAgent.includes('Prerender')) sheet.speedy(false)
