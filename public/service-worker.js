3c34580f563305cc65c0c064c0e7da31799530e0
Almog Gabay <theninesky@gmail.com>
2022-11-17 00:08:42 +0200

    Redirect every page request to '/' to get a 304 status

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,7 @@
-self.addEventListener('fetch', () => {})
-
 self.addEventListener('install', self.skipWaiting)
+
+self.addEventListener('fetch', event => {
+  if (event.request.destination === 'document') {
+    event.respondWith(fetch(new Request(self.registration.scope)))
+  }
+})
