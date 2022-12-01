f9616aceab3c85e645d7afc7051338120f130000
Almog Gabay <theninesky@gmail.com>
2022-12-01 20:37:24 +0200

    Transition 'Back' component navigations

--- src/components/common/Back.tsx
+++ src/components/common/Back.tsx
@@ -1,12 +1,24 @@
 import type { FC } from 'react'
 import { Link, LinkProps } from 'react-router-dom'
+import { useDelayedNavigate } from 'frontend-essentials'
 import { css, cx } from '@emotion/css'
 
 import BackIcon from 'images/back.svg'
 
-const Back: FC<LinkProps> = ({ className, ...otherProps }) => {
+const Back: FC<LinkProps> = ({ className, to, onClick, ...otherProps }) => {
+  const navigate = useDelayedNavigate()
+
   return (
-    <Link className="flex" {...otherProps}>
+    <Link
+      className="flex"
+      to={to}
+      onClick={event => {
+        event.preventDefault()
+        navigate(to)
+        onClick?.(event)
+      }}
+      {...otherProps}
+    >
       <BackIcon className={cx(style.backIcon, className)} />
     </Link>
   )
