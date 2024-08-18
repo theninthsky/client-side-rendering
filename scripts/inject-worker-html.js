1972efc5b36fdeb0737591f21f93a6ae2315b753
Almog Gabay <theninesky@gmail.com>
2024-08-18 12:49:40 +0300

    Remove pages scripts in CF worker

--- scripts/inject-worker-html.js
+++ scripts/inject-worker-html.js
@@ -7,11 +7,12 @@ const assets = readFileSync(join(__dirname, '..', 'public', 'assets.js'), 'utf-8
 let html = readFileSync(join(__dirname, '..', 'build', 'index.html'), 'utf-8')
 let worker = readFileSync(join(__dirname, '..', 'build', '_worker.js'), 'utf-8')
 
-html = html.replace(/scripts\.forEach[\s\S]*?data\?\.\s*forEach/, () => 'data?.forEach')
-html = html.replace(/preloadAssets/g, () => 'preloadData')
+html = html
+  .replace(/,"scripts":\s*\[(.*?)\]/g, () => '')
+  .replace(/scripts\.forEach[\s\S]*?data\?\.\s*forEach/, () => 'data?.forEach')
+  .replace(/preloadAssets/g, () => 'preloadData')
 
-worker = worker.replace('INJECT_ASSETS_HERE', () => assets)
-worker = worker.replace('INJECT_HTML_HERE', () => JSON.stringify(html))
+worker = worker.replace('INJECT_ASSETS_HERE', () => assets).replace('INJECT_HTML_HERE', () => JSON.stringify(html))
 
 rmSync(join(__dirname, '..', 'public', 'assets.js'))
 writeFileSync(join(__dirname, '..', 'build', '_worker.js'), worker)
