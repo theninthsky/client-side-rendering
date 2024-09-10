888cddb742243b7c32948f59a6175c561c45b01e
Almog Gabay <theninesky@gmail.com>
2024-09-10 19:54:28 +0300

    Rspack (#10)
    * Start migrating to rspack
    
    * Get rspack to work in dev
    
    * Replace 'workbox-webpack-plugin' with '@aaroon/workbox-rspack-plugin'
    
    * Update @aaroon/workbox-rspack-plugin
    
    * Update dependencies

--- rspack.config.js
+++ rspack.config.js
@@ -1,11 +1,10 @@
 import { join, resolve } from 'node:path'
 import { writeFileSync } from 'node:fs'
-import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin'
-import ForkTsCheckerPlugin from 'fork-ts-checker-webpack-plugin'
+import rspack from '@rspack/core'
+import ReactRefreshPlugin from '@rspack/plugin-react-refresh'
 import ESLintPlugin from 'eslint-webpack-plugin'
-import { InjectManifest } from 'workbox-webpack-plugin'
+import { InjectManifest } from '@aaroon/workbox-rspack-plugin'
 import HtmlPlugin from 'html-webpack-plugin'
-import CopyPlugin from 'copy-webpack-plugin'
 
 import pagesManifest from './src/pages.js'
 import htmlTemplate from './public/index.js'
@@ -21,7 +20,6 @@ export default (_, { mode }) => {
       port: 3000,
       devMiddleware: { stats: 'errors-warnings' }
     },
-    cache: { type: 'filesystem' },
     devtool: production ? 'source-map' : 'inline-source-map',
     resolve: {
       modules: [resolve(__dirname, 'src'), 'node_modules'],
@@ -39,12 +37,12 @@ export default (_, { mode }) => {
           test: /\.(t|j)sx?$/i,
           exclude: /(node_modules)/,
           use: {
-            loader: 'swc-loader',
+            loader: 'builtin:swc-loader',
             options: {
               jsc: {
                 parser: { syntax: 'typescript', tsx: true },
                 transform: {
-                  react: { runtime: 'automatic', refresh: true }
+                  react: { runtime: 'automatic', development: !production, refresh: !production }
                 },
                 target: 'es2017',
                 preserveAllComments: true
@@ -99,14 +97,11 @@ export default (_, { mode }) => {
             swType =>
               new InjectManifest({
                 include: [/fonts\//, /scripts\/.+\.js$/],
-                swSrc: join(__dirname, 'public', `${swType}-service-worker.js`)
+                swSrc: join(__dirname, 'public', `${swType}-service-worker.js`),
+                compileSrc: false
               })
           )
-        : [
-            new ReactRefreshPlugin(),
-            new ForkTsCheckerPlugin(),
-            new ESLintPlugin({ extensions: ['js', 'ts', ' jsx', 'tsx'] })
-          ]),
+        : [new ReactRefreshPlugin(), new ESLintPlugin({ extensions: ['js', 'ts', ' jsx', 'tsx'] })]),
       new HtmlPlugin({
         scriptLoading: 'module',
         templateContent: ({ compilation }) => {
@@ -134,7 +129,7 @@ export default (_, { mode }) => {
           return htmlTemplate(pages)
         }
       }),
-      new CopyPlugin({
+      new rspack.CopyRspackPlugin({
         patterns: [
           {
             from: 'public',
